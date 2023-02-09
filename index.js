de94a2416487a0edad4f171277e4323c354bfba1
Votybe <66923802+Votybe@users.noreply.github.com>
2023-02-09 01:00:45 +0100

    test

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.get("/flip-coin", (req, res) => {
   res.send(coin);
 });
 
-console.log("Hello World!");
-
 app.get("/flip-coins", (req, res) => {
   const times = req.query.times;
   if (times && times > 0) {
